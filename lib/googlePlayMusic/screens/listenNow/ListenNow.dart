ca180a63f758f17a19304c7fa21ca8686fd9b9bf
helio <heliojmsemedo@gmail.com>
2019-07-15 01:33:49 +0100

    started work on ListenNow grid...what a mess

--- lib/googlePlayMusic/screens/listenNow/ListenNow.dart
+++ lib/googlePlayMusic/screens/listenNow/ListenNow.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter_app/googlePlayMusic/Drawer.dart';
 import 'package:flute_music_player/flute_music_player.dart';
 import 'package:flutter_redux/flutter_redux.dart';
@@ -11,6 +15,30 @@ class ListenNow extends StatefulWidget {
 
   @override
   State createState() => new ListenNowState();
+}
+
+class HolyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 26,
+      width: 52,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(2.0),
+            color: Theme.of(context).primaryColor,
+            shape: BoxShape.rectangle),
+        child: Center(
+          child: Text(
+            'MORE',
+            maxLines: 1,
+            style: TextStyle(
+                fontSize: 13, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class ListenNowState extends State<ListenNow> {
@@ -61,21 +89,79 @@ class ListenNowState extends State<ListenNow> {
         drawer: Drawer(child: MyDrawer()),
         body: Container(
             padding: new EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Flexible(
                     child: ListTile(
-                  title: Text("Recent activity",
-                      style: Theme.of(context).textTheme.title),
-                  subtitle: Text("Recently played or added"),
-                  trailing: SizedBox(height: 22, width: 45, child:FlatButton(
-                      child: Text('MORE', style: TextStyle(fontSize:4, color: Colors.white),),
-                      onPressed: () {},
-                      color: Theme.of(context).primaryColor,),
-                      
-                )))
+                        title: Text("Recent activity",
+                            style: Theme.of(context).textTheme.title),
+                        subtitle: Text("Recently played or added"),
+                        onTap: () {},
+                        trailing: HolyButton())),
+                Flexible(
+                    child: Row(
+                  children: <Widget>[
+                    Flexible(
+                        child: Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Flexible(
+                              child: Image(
+                            image: FileImage(File(widget.songs
+                                .where((i) => i.albumArt != null)
+                                .toList()[Random().nextInt(100)]
+                                .albumArt)),
+                            fit: BoxFit.contain,
+                          )),
+                          Text("Shuffle all")
+                        ],
+                      ),
+                    )),
+                    Flexible(
+                        child: Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Flexible(
+                              child: Image(
+                            image: FileImage(File(widget.songs
+                                .where((i) => i.albumArt != null)
+                                .toList()[Random().nextInt(100)]
+                                .albumArt)),
+                            fit: BoxFit.contain,
+                          )),
+                          Text("Shuffle all")
+                        ],
+                      ),
+                    )),
+                    Flexible(
+                        child: Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Flexible(
+                              child: Image(
+                            image: FileImage(File(widget.songs
+                                .where((i) => i.albumArt != null)
+                                .toList()[Random().nextInt(100)]
+                                .albumArt)),
+                            fit: BoxFit.contain,
+                          )),
+                          Text("Shuffle all")
+                        ],
+                      ),
+                    )),
+                  ],
+                ))
               ],
             )));
   }
@@ -116,5 +202,35 @@ class ListenNowState extends State<ListenNow> {
             builder: (context, viewModel) {
               return Scrollbar(child: ListView(children: viewModel));
             }));
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Grid List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline,
+              ),
+            );
+          }),
+        ),
+      ),
+    );
   }
 }
